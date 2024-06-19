ea40159b47744270cec29cb99fa40a3c8a81c13f
jinkshower <askthem@naver.com>
2024-06-19 19:38:50 +0900

    feat: userdao findall update 구현

--- src/main/java/next/controller/ListUserController.java
+++ src/main/java/next/controller/ListUserController.java
@@ -1,19 +1,35 @@
 package next.controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import core.db.DataBase;
 import core.mvc.Controller;
+import next.dao.UserDao;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ListUserController implements Controller {
+    private static Logger log = LoggerFactory.getLogger(ListUserController.class);
+
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         if (!UserSessionUtils.isLogined(req.getSession())) {
             return "redirect:/users/loginForm";
         }
+        UserDao userDao = new UserDao();
+        List<User> all = new ArrayList<>();
+        try {
+            all = userDao.findAll();
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+        }
 
-        req.setAttribute("users", DataBase.findAll());
+        req.setAttribute("users", all);
         return "/user/list.jsp";
     }
 }
