f966aab8ddfd981851d13a50f5f64059c7af1737
jinkshower <askthem@naver.com>
2024-06-21 11:46:23 +0900

    feat: ajax를 통한 답변 동적 html 생성

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -8,3 +8,29 @@ String.prototype.format = function() {
   });
 };
 
+$(".answerWrite input[type=submit]").click(addAnswer);
+
+function addAnswer(e) {
+  e.preventDefault(); // submit이 자동 동작하는 것을 막음
+  // form 데이터들을 자동으로 묵어준다.
+  var queryString = $("form[name=answer]").serialize();
+
+  $.ajax({
+    type: 'post',
+    url: '/api/qna/addAnswer',
+    data: queryString,
+    dataType: 'json',
+    error: onError,
+    success: onSuccess,
+  });
+}
+
+function onSuccess(json, status) {
+  var answerTemplate = $("#answerTemplate").html();
+  var template = answerTemplate.format(json.writer, new Date(json.createdDate), json.contents, json.answerId);
+  $(".qna-comment-slipp-articles").prepend(template);
+}
+
+function onError(xhr, status) {
+  alert("error");
+}
