35567ed94849cc9c45ec57ce4dc934de09eceaf2
jinkshower <askthem@naver.com>
2024-06-24 13:19:49 +0900

    feat: AnnotationHandlerMapping 구현 (주석 참고)

--- src/main/java/core/nmvc/AnnotationHandlerMapping.java
+++ src/main/java/core/nmvc/AnnotationHandlerMapping.java
@@ -1,14 +1,19 @@
 package core.nmvc;
 
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.google.common.collect.Maps;
-
+import com.google.common.collect.Sets;
+import core.annotation.RequestMapping;
 import core.annotation.RequestMethod;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import org.reflections.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AnnotationHandlerMapping {
+    private static final Logger logger = LoggerFactory.getLogger(AnnotationHandlerMapping.class);
     private Object[] basePackage;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
@@ -18,7 +23,35 @@ public class AnnotationHandlerMapping {
     }
 
     public void initialize() {
+        ControllerScanner controllerScanner = new ControllerScanner(basePackage);
+        //controller scanner로 controller 클래스를 모두 찾는다.
+        Map<Class<?>, Object> controllers = controllerScanner.getControllers();
+        //RequestMapping이 붙은 메서드를 모두 찾는다
+        Set<Method> methods = getRequestMappingMethods(controllers.keySet());
 
+        for (Method method : methods) {
+            //메서드에서 Requestmapping에 설정한 값을 찾는다
+            RequestMapping rm = method.getAnnotation(RequestMapping.class);
+            logger.debug("register handlerExecution : url is {}, method is {}", rm.value(), method);
+            handlerExecutions.put(createHandlerKey(rm), new HandlerExecution(controllers.get(method.getDeclaringClass()), method));
+        }
+    }
+
+    private HandlerKey createHandlerKey(final RequestMapping rm) {
+        //어노테이션에 선언된 url, http method로 만든다
+        return new HandlerKey(rm.value(), rm.method());
+    }
+
+    @SuppressWarnings("unchecked")
+    private Set<Method> getRequestMappingMethods(final Set<Class<?>> classes) {
+        //Controller 어노테이션이 붙은 클래스에서 RequestMapping어노테이션이 붙은 메서드들을 찾음
+        Set<Method> requestMappingMethods = Sets.newHashSet();
+        for (Class<?> clazz : classes) {
+            Set<Method> allMethods = ReflectionUtils.getAllMethods(clazz,
+                ReflectionUtils.withAnnotation(RequestMapping.class));
+            requestMappingMethods.addAll(allMethods);
+        }
+        return requestMappingMethods;
     }
 
     public HandlerExecution getHandler(HttpServletRequest request) {
