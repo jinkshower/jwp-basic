8f8d5b027950f619da6bcffd7419e2f3feafa0ee
jinkshower <askthem@naver.com>
2024-06-25 11:21:53 +0900

    feat: @Inject로 생성

--- src/main/java/core/di/factory/BeanFactory.java
+++ src/main/java/core/di/factory/BeanFactory.java
@@ -1,5 +1,8 @@
 package core.di.factory;
 
+import com.google.common.collect.Lists;
+import java.lang.reflect.Constructor;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -7,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.Maps;
+import org.springframework.beans.BeanUtils;
 
 public class BeanFactory {
     private static final Logger logger = LoggerFactory.getLogger(BeanFactory.class);
@@ -25,6 +29,52 @@ public class BeanFactory {
     }
 
     public void initialize() {
+        //preInstantiateBeans 에는 controller service repository 어노테이션이 붙은 클래스가 있다.
+        for (Class<?> clazz : preInstanticateBeans) {
+            //beans에 clazz가 없으면 instantiateClass를 호출한다.
+            if (beans.get(clazz) == null) {
+                instantiateClass(clazz);
+            }
+        }
+    }
 
+    private Object instantiateClass(Class<?> clazz) {
+        Object bean = beans.get(clazz);
+        if (bean != null) {
+            return bean;
+        }
+        //@Inject가 붙은 생성자를 찾는다
+        Constructor<?> injectedConstructor = BeanFactoryUtils.getInjectedConstructor(clazz);
+        if (injectedConstructor == null) {
+            //없으면 기본 생성자로 바로 생성한다.
+            bean = BeanUtils.instantiate(clazz);
+            beans.put(clazz, bean);
+            return bean;
+        }
+
+        logger.debug("Constructor : {}", injectedConstructor);
+        //@Inject가 붙은 생성자를 호출하여 인스턴스를 생성한다.
+        bean = instantiateConstructor(injectedConstructor);
+        beans.put(clazz, bean);
+        return bean;
+    }
+
+    private Object instantiateConstructor(Constructor<?> constructor) {
+        //@Inject가 아닌 기본생성자로 생성할 수 있을 때까지 재귀를 실행한다.
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        List<Object> args = Lists.newArrayList();
+        for (Class<?> clazz : parameterTypes) {
+            Class<?> concreteClazz = BeanFactoryUtils.findConcreteClass(clazz, preInstanticateBeans);
+            if (!preInstanticateBeans.contains(concreteClazz)) {
+                throw new IllegalStateException(clazz + "는 Bean이 아니다");
+            }
+
+            Object bean = beans.get(concreteClazz);
+            if (bean == null) {
+                bean = instantiateClass(concreteClazz);
+            }
+            args.add(bean);
+        }
+        return BeanUtils.instantiateClass(constructor, args.toArray());
     }
 }
